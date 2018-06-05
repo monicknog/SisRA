e5782bbd1754fd6200756d8fe9a1450210d1dc74
monicknog <monickhnm@gmail.com>
2018-06-05 13:15:02 -0300

    Biblioteca e arquivo arduino

---
+++ rfid_read.ino
@@ -0,0 +1,28 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+}
+
+void loop() {
+  if(!mfrc522.PICC_IsNewCardPresent())
+    return;
+
+  if(!mfrc522.PICC_ReadCardSerial())
+    return;
+
+   for(byte i = 0; i < mfrc522.uid.size; i++){
+      Serial.print(mfrc522.uid.uidByte[i] < 0x10?"0":"");
+      Serial.print(mfrc522.uid.uidByte[i], HEX);
+   } 
+   Serial.println();
+   delay(600);
+}
